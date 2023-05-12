26ff1f26d115f5a0d7a73a5b6310ba0d56992bfc
HamadaSalah <hamada.mahmoud880@gmail.com>
2023-05-12 20:20:57 +0300

    first commit

---
+++ resources/views/IBDP.blade.php
@@ -0,0 +1,276 @@
+@extends('layouts.app')
+@section('content')
+<div class="IBDP_page">
+    
+    <div class="w-100 z-n1">
+        <img class="w-100" src="assets/images/Asset.png" alt="">
+    </div>
+    <div class="w-100 z-n1">
+        <img class="w-100" src="assets/images/Assets.png" alt="">
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-end">
+            <div class="col-12 col-md-6 col-lg-3">
+                <img src="assets/images/IBDP.png" alt="" class="img-fluid rounded-3">
+            </div>
+        </div>
+    </div>
+
+    <div class="container mb-5">
+        <div class="row flex-column-reverse flex-md-column-reverse flex-lg-row-reverse flex-xl-row-reverse flex-xxl-row-reverse mt-5">
+            <div class="col-12 col-md-12 col-lg-6 misimg">
+                <img src="assets/images/Website pix/IBDP/5Q7C3512.jpg" alt="" class="h-100 rounded-3">
+            </div>
+            <div class="col-12 col-md-12 col-lg-6 mt-4">
+                <h2 class="pb-3 m-0 Open_Sans_font">IBO Mission Statement</h2>
+
+                <p class="pb-3 lh-lg m-0 Open_Sans_font fw-semibold">
+                    The International Baccalaureate aims to develop inquiring,
+                    knowledgeable and caring young people who help to create a
+                    better and more peaceful world through intercultural
+                    understanding and respect.
+                </p>
+                <p class="pb-3 lh-lg m-0 Open_Sans_font fw-semibold">
+                    To this end the IB works with schools, governments and
+                    international organizations to develop challenging programmes
+                    of international education and rigorous assessment.
+                </p>
+                <p class="pb-3 lh-lg m-0 Open_Sans_font fw-semibold">
+                    These programmes encourage students across the world to
+                    become active, compassionate and lifelong learners who
+                    understand that other people, with their differences, can also
+                    be right.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mb-5">
+        <div class="row flex-column-reverse flex-md-column-reverse flex-lg-row flex-xl-row flex-xxl-row mt-5">
+            <div class="col-12 col-md-12 col-lg-3 mt-4">
+                <img src="assets/images/Website pix/IBDP/IYYW1606.jpg" alt="" class="w-100 rounded-3">
+            </div>
+            <div class="col-12 col-md-12 col-lg-9 mt-4">
+                <h2 class="pb-4 m-0 Open_Sans_font">About the IBDP programme</h3>
+
+                    <p class="pb-3 lh-lg m-0 Open_Sans_font">
+                        Students in the Secondary section are being prepared for the International
+                        Baccalaureate Diploma Programme. At different points in the upper grades;
+                        students will start their journey into the IB with activities in ATL Skills, internal
+                        assessments, CAS and TOK.
+                    </p>
+                    <p class="pb-3 lh-lg m-0 Open_Sans_font">
+                        The IB Programme is known and recognized worldwide as the finest pre-university
+                        education available. As a result, students who graduate with IB credentials have the
+                        choice to enter Jordanian universities or most universities aboard.
+                    </p>
+                    <br><br>
+                    <div class="d-flex justify-content-end me-5 mt-5">
+                        <a class="fs-7 py-1 px-4 first_color Open_Sans_font fw-bold text-capitalize rounded-2 hover_btn _borders "
+                           href="https://test.cambridge.edu.jo/unver-cons">UNIVERSITY COUNSELLING</a>
+                    </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="w-100 mt-5">
+        <img class="w-100" src="assets/images/Assets8.png" alt="">
+    </div>
+
+    <div class="second_color_bg margin-negative py-5">
+        <div class="container">
+            <div class="row flex-column-reverse flex-md-column-reverse flex-lg-row-reverse flex-xl-row-reverse flex-xxl-row-reverse mt-5">
+                <div class="col-12 col-md-12 col-lg-6 mt-4">
+                    <div class="row">
+                        <div class="col-12 col-md-12 col-lg-6">
+                            <div class="d-flex flex-column">
+                                <img src="assets/images/Website pix/IBDP/5Q7C5247.jpg" alt=""
+                                     class="h-100 mb-4 rounded-3">
+                                <img src="assets/images/Website pix/IBDP/051.jpg" alt=""
+                                     class="h-100 mb-4 rounded-3">
+                                <img src="assets/images/Website pix/IBDP/5Q7C3512.jpg" alt=""
+                                     class="h-100 mb-4 mb-lg-0 mb-xl-0 mb-xxl-0 rounded-3">
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-12 col-lg-6">
+                            <img src="assets/images/Website pix/IBDP/IYYW1606.jpg" alt="" class="h-100 rounded-3">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-12 col-lg-6 mt-4">
+                    <h2 class="pb-3 m-0 Open_Sans_font">Admission into the IBDP</h2>
+
+                    <p class="pb-3 lh-lg m-0 Open_Sans_font fw-semibold">
+                        Admission into the IBDP starts with an application where
+                        subject choices and course of study are selected. All
+                        students are encouraged to try the diploma option.
+                        Teachers give feedback concerning the ability of students
+                        who selected a subject. Each individual student will be
+                        interviewed by the IBDP coordinator and/or head of
+                        school to check the compatibility of student with chosen
+                        programme, MOE requirements and future vocational
+                        requirements.
+                    </p>
+                    <div class="">
+                        <p class="pb-3 lh-lg m-0 w-content-75 Open_Sans_font fw-semibold">
+                            For the 2019/ 2020 academic year CHS offers the
+                            following courses and levels:
+                        </p>
+                        <div class="d-flex flex-column flex-md-row flex-lg-row flex-xl-row flex-xxl-row justify-content-between align-items-start align-items-md-center align-items-lg-center align-items-xl-center align-items-xxl-center">
+                            <ul class="d-flex flex-column">
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        Business Management HL/ SL
+                                    </p>
+                                </li>
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        Arabic Language & Literature SL
+                                    </p>
+                                </li>
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        English Language & Literature SL
+                                    </p>
+                                </li>
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        Analysis and Approaches in Math SL
+                                    </p>
+                                </li>
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        Applications and Interpretations in Math SL/ HL
+                                    </p>
+                                </li>
+                            </ul>
+                            <ul class="d-flex flex-column ps-3 me-2 border-bottom-0 border-end-0 border-top-0 _borders">
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        Visual Art HL/ SL
+                                    </p>
+                                </li>
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        Chemistry HL/ SL
+                                    </p>
+                                </li>
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        Economics HL/ SL
+                                    </p>
+                                </li>
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        English B HL/ SL
+                                    </p>
+                                </li>
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        Arabic B HL/ SL
+                                    </p>
+                                </li>
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        Biology HL/ SL
+                                    </p>
+                                </li>
+                                <li class="pb-2">
+                                    <p class="lh-lg m-0 Open_Sans_font fw-semibold">
+                                        History SL
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mb-5">
+        <div class="row flex-column-reverse flex-md-column-reverse flex-lg-row flex-xl-row flex-xxl-row mt-5">
+            <div class="col-12 col-md-12 col-lg-6 mt-4 misimg">
+                <img src="assets/images/Website pix/IBDP/5Q7C3512.jpg" alt="" class="h-100 rounded-3">
+            </div>
+            <div class="col-12 col-md-12 col-lg-6 mt-4">
+                <h2 class="pb-3 m-0 Open_Sans_font">CHS three elements</h2>
+
+                <p class="pb-3 lh-lg m-0 Open_Sans_font fw-semibold">
+                    Strongly committed to the principle of developing the
+                    whole person, the IB believes that this is best achieved by
+                    identifying and developing clearer and more explicit aims
+                    for and relationships between TOK, CAS and the
+                    extended essay. Support the interconnectedness of
+                    learning.
+                </p>
+                <p class="pb-3 lh-lg m-0 Open_Sans_font fw-semibold">
+                    All three elements of the core should be grounded in
+                    three coherent aims:
+                    Support, and be supported by, the academic disciplines.
+                    Foster international-mindedness.
+                    Develop self-awareness and a sense of identity.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="second_color_bg py-5">
+        <div class="container mb-5">
+            <div class="row flex-column-reverse flex-md-column-reverse flex-lg-row-reverse flex-xl-row-reverse flex-xxl-row-reverse mt-5">
+                <div class="col-12 col-md-12 col-lg-6 mt-4 misimg">
+                    <img src="assets/images/Website pix/IBDP/5Q7C7632.jpg" alt="" class="h-100 rounded-3">
+                </div>
+                <div class="col-12 col-md-12 col-lg-6 mt-4">
+                    <h2 class="pb-3 m-0 Open_Sans_font">Fostering international-mindedness</h2>
+
+                    <p class="pb-3 lh-lg m-0 Open_Sans_font fw-semibold">
+                        The core has a responsibility to foster and nurture
+                        international-mindedness, with the ultimate goal of developing
+                        responsible global citizens. To a large extent, the core should
+                        be driven by the IB’s mission “to develop inquiring,
+                        knowledgeable and caring young people who help to create a
+                        better and more peaceful world through intercultural
+                        understanding and respect” and “encourage students across
+                        the world to become active, compassionate and lifelong
+                        learners who understand that other people, with their
+                        differences, can also be right” (IB mission statement).
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mb-5">
+        <div class="row flex-column-reverse flex-md-column-reverse flex-lg-row-reverse flex-xl-row-reverse flex-xxl-row-reverse mt-5">
+
+            <div class="col-12 mt-4">
+                <h2 class="pb-3 m-0 Open_Sans_font">What is CAS and TOK</h2>
+
+                <p class="pb-3 lh-lg m-0 Open_Sans_font fw-semibold">
+                    TOK guides students in making sense of their experiences as learners, and this includes their
+                    experiences in CAS.
+                </p>
+                <p class="pb-3 lh-lg m-0 Open_Sans_font fw-semibold">
+                    CAS experiences are an important source of students’ personal knowledge, providing students with
+                    the opportunity
+                    to gain awareness of the world in a range of diverse and challenging situations. Shared
+                    knowledge extends the idea
+                    from how individuals construct knowledge to how communities construct knowledge. In CAS,
+                    students might draw on
+                    TOK discussions that deepen understanding of different communities and cultures.
+                </p>
+
+                <div class="d-flex justify-content-end me-5 mt-5">
+                    <a class="fs-7 py-1 px-4 first_color Open_Sans_font fw-bold text-capitalize rounded-2 hover_btn _borders "
+                       href="{{route('CAS')}}">CAS</a>
+                </div>
+            </div>
+            <div class="d-flex justify-content-end me-5 mt-5">
+                <a class="fs-7 py-1 px-4 first_color Open_Sans_font fw-bold text-capitalize rounded-2 hover_btn _borders " href="https://test.cambridge.edu.jo/tuition-fees">APPLY NOW</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/unver_cons.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+@section('content')
+
+
+ <div class="w-100 z-n1">
+        <img class="w-100" src="assets/images/Asset.png" alt="">
+    </div>
+<div class="container">
+    
+   
+    <div class="row">
+        <div class="unver">
+            <h1 class="text-center mt-3"> University Counseling</h1>
+            
+            
+            
+            
+            <p>
+
+Cambridge high school is committed to supporting each of it’s students in the journey of intellectual and personal growth to be able to thrive at the university level and beyond.  <br>The core of the university counseling mission is to help students find appropriate matches for their strengths, interests and future goals within higher education.
+ <br>
+The most obvious goal of university counseling is to help students identify, apply, secure admissions to colleges and universities that meet their needs, nurture talents and challenge them. Equally important is the development of decision-making skills, time management skills, refining social and emotional intelligence within the process. Through university counseling we help empower students to make informed decisions, advocate for themselves, follow a timeline, meet internal deadlines and employ strong organizational skills throughout the demanding application process.
+ <br>
+Our intention is to provide timely information, counseling, and support to help develop the necessary skills to complete each phase of the university process successfully.  <br>We take pride in our welcoming approach to both students and parents, our strong relationships with universities and the quality of guidance and advocacy provided. You will receive professional advice, yet no one will make choices on your behalf.
+ 
+ <br>Ideally, early planning comes a very long way. We will be thinking out loud as early as Grade 10, solidify goals in Grade 11. And submit applications during the first two months of Grade 12.
+  <br> <ul style="list-style-type:disc;">
+<li>	Here’s a list of Do’s and don’ts:</li>
+<li>	CV Sample</li>
+<li>	Suitable timeline:</li>
+<li>	Important terminologies to know:</li>
+	<li>Students Role:</li>
+<li>	General checklist:</li></ul> </p>
+
+
+
+        </div>
+       
+    </div>
+</div>
+
+@endsection
